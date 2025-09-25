5af3705d559c9b2f45a55666a43193050cad48f7
Saulo <saulo.ftu@gmail.com>
2025-09-25 13:25:08 -0300

    Objetos

---
+++ Introduçao Objetos/index.js
@@ -0,0 +1,43 @@
+
+//Posso utilizar CONST e mudar valores internos do Objeto
+const array = ['Saulo','Gomes','Fonseca']
+array.push('João','Fernando');
+array[0] = 'Claudio'
+array.pop();
+
+// NÃO POSSO MUDAR O OBJETO PARA ALGO DIFERENTE (POR SER UMA CONST):  array = 'Saulo' X
+console.log(array)
+
+// variaveis relacionadas
+const nome01 = 'Saulo';
+const sobrenome01 = 'Gomes';
+const idade01 = 25;
+
+//Posso criar um objeto para facilitar
+
+// const pessoa01 = {
+//     nome: 'Saulo', 
+//     sobrenome: 'Gomes', 
+//     Idade:25
+// };
+
+
+
+function criaPessoa (nome,sobrenome,idade){
+    return {nome,sobrenome,idade};
+};
+
+const pessoa01 = criaPessoa('Saulo','Gomes',25);
+console.log(pessoa01.nome)
+
+const pessoa02 = {
+    nome:'Maria',
+    sobrenome:'Claudiane',
+    idade:25,
+
+    fala(){
+        console.log(`${this.nome} ${this.sobrenome} está mandando um oi!`)
+    }
+};
+
+pessoa02.fala();
