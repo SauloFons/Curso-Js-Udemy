ed438d2235ac8657d8b87532448f8cd9dc852a96
Saulo <saulo.ftu@gmail.com>
2025-09-25 12:48:18 -0300

    Conclusão aula de funções

--- Intrudção Funções/index.js
+++ Intrudção Funções/index.js
@@ -1,5 +1,31 @@
+//declaração de funçao
 function saudacao(nome){
     console.log(`Boa tarde ${nome}`)
 }
 
 saudacao("Saulo");
+
+function soma (x,y){
+    const result = (x+y);
+    return result
+}
+
+console.log(soma(12,13));
+
+//função anônima
+const raizQuadrada = function (n){
+    return (n **0.5);
+};
+
+console.log(raizQuadrada(9))
+
+
+// arrow function 
+/*
+Quando temos só uma linha de função, posso elimitar o ";", as chaves "{}" e
+o return.
+Se a função pede somente um parametro, posso tirar os parenteses '()' de recebimento
+de parametros.
+*/
+
+const raizQ = n => n**0.5;
