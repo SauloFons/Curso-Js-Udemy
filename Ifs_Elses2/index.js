59f1567eac9de360896641eb212655e72d20305b
Saulo <saulo.ftu@gmail.com>
2025-10-01 15:45:50 -0300

    aula 2 if /else

---
+++ Ifs_Elses2/index.js
@@ -0,0 +1,11 @@
+const numero = (5);
+
+if (numero >= 0 && numero <5){
+    console.log('Numero está entre 0 e 5');
+} else if (numero >5 && numero<=10){ 
+    console.log('Número está entre 5 e 10');
+} else if (numero === 5){
+    console.log ('O número é 5');
+} else {
+    console.log ('Número não é entro 0 e 10')
+}
