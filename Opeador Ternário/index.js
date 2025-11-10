3bc074875c62b1fc38c029237515631b85da1aae
Saulo <saulo.ftu@gmail.com>
2025-11-10 11:18:47 -0300

    Criando Modelo base

---
+++ Opeador Ternário/index.js
@@ -0,0 +1,18 @@
+const pontuacaoUsuario = 999;
+
+// (condição) ? 'Valor para verdadeiro' : 'Valor para falso';
+
+const nivelUsuario = pontuacaoUsuario >=1000 ? 'Usuário VIP' : 'Usuário normal';
+console.log(nivelUsuario)
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preto';
+
+
+
+// if(pontuacaoUsuario >= 1000){
+//     console.log("Usuário vip");
+// } else{
+//     console("Usuário comum");
+// }
+
