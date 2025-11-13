1b5688f2126256a19092d45455b5f4477bc0d7f2
Saulo <saulo.ftu@gmail.com>
2025-11-12 21:07:17 -0300

    Laço de repetição FOR

---
+++ Repetição_For/index.js
@@ -0,0 +1,12 @@
+// for (let i = 0; i<=5 ; i++){
+//     console.log (`Linha ${i}`)
+// }
+
+const frutas = ['Maçã' , 'Pêra', 'Uva'];
+
+for (let i = 0 ; i < frutas.length; i++){
+    console.log(frutas[i])
+}
+
+
+
