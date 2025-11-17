48923b9fd28aa1c19629b2b077bec25e1d1812fe
Saulo <saulo.ftu@gmail.com>
2025-11-17 13:14:12 -0300

    Exercício_5

--- Exercício_5/main.js
+++ Exercício_5/main.js
@@ -1,2 +1,14 @@
 //Escreva um função chamada ePaisagem que recebe dois argumentos. largura e altura de uma imagem (number).
 //Retorne true se a imagem estiver no modo paisagem 
+
+//MINHA SOLUÇÃO 
+
+const ePaisagem = (largura,altura) => largura > altura ? true: false;
+console.log(ePaisagem(1920,1080))
+
+
+//SOLUÇÃO DO PROFESSOR 
+
+
+const ePaisagem2 = (largura,altura) => largura > altura
+console.log(ePaisagem2(1920,1080))
