59d75260a59aeb9cc327f51f0e887ac8ed56d8aa
Saulo <saulo.ftu@gmail.com>
2025-11-21 13:49:37 -0300

    Controladora com Constructor Function

---
+++ Calculadora/assets/js/main.js
@@ -0,0 +1,36 @@
+function Calculadora() {
+
+    this.display = document.querySelector('.display')
+
+    this.inicia = () => this.capturaCliques();
+
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+            const el = event.target
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clear();
+            if (el.classList.contains('btn-del')) this.del();
+            if (el.classList.contains('btn-eq')) this.realizaConta();
+        });
+    };
+
+    this.clear = () => this.display.value = '';
+    this.addNumDisplay = (el) => this.display.value += el.innerText;
+    this.del = () => this.display.value = this.display.value.slice(0, -1)
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value)
+            if (!conta) {
+                alert('Conta inválida');
+                return
+            }
+            this.display.value = conta;
+        } catch (e) {
+            alert('Conta inválida');
+            return
+        };
+    }
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
