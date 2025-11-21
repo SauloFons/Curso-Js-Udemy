e28b118fe4c454e08d503186ecf104efa312eb3c
Saulo <saulo.ftu@gmail.com>
2025-11-21 17:47:16 -0300

    Concatenação de Arryas (Concat e Spread operator)

--- Array_Avançado/main.js
+++ Array_Avançado/main.js
@@ -17,21 +17,29 @@
 
 // ******************************************************************************************************************
 
-const names = ['Silas','Almeida','Gilberto','Santos','Claudia']
-//names.splice(indice , delete, elementos )
-//pop 
-// const removidos = names.splice(-2 , Number.MAX_VALUE)
-// console.log(names)
+// const names = ['Silas','Almeida','Gilberto','Santos','Claudia']
+// //names.splice(indice , delete, elementos )
+// //pop 
+// // const removidos = names.splice(-2 , Number.MAX_VALUE)
+// // console.log(names)
 
-const removidos = names.splice(2 , 1 , 'Claudio')
-console.log(removidos)
-console.log(names)
+// const removidos = names.splice(2 , 1 , 'Claudio')
+// console.log(removidos)
+// console.log(names)
 
 // ******************************************************************************************************************
 
 
 
+const a1 = [1,2,3];
+const a2 = [4,5,6];
+//const a3 = a1 + a2;
+// const a3 = a1.concat(a2, [7,8,9],'Saulo');
 
+//... Rest operator, ...Spread operator
+
+const a3 = [...a1,...a2, ...[7,8,9] ,'Saulo']
+console.log(a3);
 
 
 
