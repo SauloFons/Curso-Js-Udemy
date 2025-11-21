807dfc9fe79fb866daa8f5494e238569894f2e4d
Saulo <saulo.ftu@gmail.com>
2025-11-21 17:42:27 -0300

    Array (Splice)

--- Array_Avançado/main.js
+++ Array_Avançado/main.js
@@ -1,16 +1,38 @@
 //VALOR POR REFERÊNCIA
 
 // const nomes = ['Saulo','Gomes','Fonseca']
-const nomes = new Array('Saulo','Gomes','Fonseca')
-const novo = [...nomes]; //COM REST OPERATOR, OS ARRAYS VIRAM INDIVIDUAIS
+// const nomes = new Array('Saulo','Gomes','Fonseca')
+// const novo = [...nomes]; //COM REST OPERATOR, OS ARRAYS VIRAM INDIVIDUAIS
 
-novo.pop();
-nomes.push('Claudia');
-novo.unshift('Wallace');
-console.log(nomes);
-console.log(novo);
-console.log(nomes.slice(1,3))
+// novo.pop();
+// nomes.push('Claudia');
+// novo.unshift('Wallace');
+// console.log(nomes);
+// console.log(novo);
+// console.log(nomes.slice(1,3))
 
-const nomes2 = 'Saulo Gomes Fonseca'
-const arrayNomes2 = nomes2.split(' ')
-console.log(arrayNomes2)
+// const nomes2 = 'Saulo Gomes Fonseca'
+// const arrayNomes2 = nomes2.split(' ')
+// console.log(arrayNomes2)
+
+// ******************************************************************************************************************
+
+const names = ['Silas','Almeida','Gilberto','Santos','Claudia']
+//names.splice(indice , delete, elementos )
+//pop 
+// const removidos = names.splice(-2 , Number.MAX_VALUE)
+// console.log(names)
+
+const removidos = names.splice(2 , 1 , 'Claudio')
+console.log(removidos)
+console.log(names)
+
+// ******************************************************************************************************************
+
+
+
+
+
+
+
+
