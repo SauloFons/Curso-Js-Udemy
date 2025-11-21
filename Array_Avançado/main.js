7940883ca232c354c460dfba7f65f3f299d238c1
Saulo <saulo.ftu@gmail.com>
2025-11-21 19:12:56 -0300

    Mapeando os Arrays (MAP)

--- Array_Avançado/main.js
+++ Array_Avançado/main.js
@@ -53,6 +53,7 @@
 // // console.log(numerosMaiores)
 
 // // *******FILTER***********
+// FILTER RETORNA POR TRUE OS VALORES QUE QUEREM SER ACRESCIDOS AO ARRAY EM QUESTÃO
 
 // // function callBackFunction(valor){
 // //     return (valor >10)
@@ -68,6 +69,43 @@
 
 
 //FILTRA PESSOAS COM O NOME MAIOR QUE 5 LETRAS
+// const pessoas = [
+//     {nome : 'Saulo' , idade: 25},
+//     {nome : 'Gomes' , idade: 67},
+//     {nome : 'Fonseca' , idade: 55},
+//     {nome : 'Ana' , idade: 32},
+//     {nome : 'Joao' , idade: 28},
+//     {nome : 'Claudio' , idade: 33},
+// ]
+
+// const pessoasNomeGrande = pessoas.filter(obj =>  obj.nome.length >=5)
+
+// console.log(pessoasNomeGrande)
+
+// //FILTRA PESSOAS COM MAIS DE 50 ANOS  
+
+// const pessoasMaisCinquenta = pessoas.filter(obj => obj.idade > 50) 
+// console.log(pessoasMaisCinquenta)
+
+// //FILTRA PESSOAS QUE O NOME TERMINCA COM "A"
+
+// const pessoasFinalA = pessoas.filter(obj => obj.nome.endsWith('a') )
+// console.log(pessoasFinalA)
+
+// *********** MAP ***********
+
+//DOBRE OS NUMEROS 
+
+// const numeros = [1 , 55 , 6 , 12 , 22 , 9 , 10 , 75 , 3 , 22 , 14 , 18];
+
+// const numerosEmDobro = numeros.map(valor => valor * 2)
+// console.log(numerosEmDobro)
+
+
+//PARA CADA ELEMENTO:
+
+//RETORNE APENAS UMA STRING COM O NOME DA PESSOA
+
 const pessoas = [
     {nome : 'Saulo' , idade: 25},
     {nome : 'Gomes' , idade: 67},
@@ -77,18 +115,20 @@ const pessoas = [
     {nome : 'Claudio' , idade: 33},
 ]
 
-const pessoasNomeGrande = pessoas.filter(obj =>  obj.nome.length >=5)
-
-console.log(pessoasNomeGrande)
-
-//FILTRA PESSOAS COM MAIS DE 50 ANOS  
-
-const pessoasMaisCinquenta = pessoas.filter(obj => obj.idade > 50) 
-console.log(pessoasMaisCinquenta)
-
-//FILTRA PESSOAS QUE O NOME TERMINCA COM "A"
-
-const pessoasFinalA = pessoas.filter(obj => obj.nome.endsWith('a') )
-console.log(pessoasFinalA)
+const nomes = pessoas.map(obj => obj.nome);
+console.log(nomes);
 
 
+//MOSTRE APENAS AS IDADES DO OBJETO
+const idade = pessoas.map(obj => ({idade : obj.idade}));
+console.log(idade);
+
+//ADICIONA UMA CHAVE DE ID EM CADA OBJETO
+
+const comId = pessoas.map(function (obj,indice){
+    const newObj = {...obj};
+    newObj.id = indice + 1
+    return newObj; 
+});
+console.log(comId)
+console.log(pessoas)
