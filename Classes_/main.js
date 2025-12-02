bfe22c10c0d6458f4822563892c39242f3565e04
Saulo <saulo.ftu@gmail.com>
2025-12-02 12:55:33 -0300

    Criação de classes

---
+++ Classes_/main.js
@@ -0,0 +1,21 @@
+class Pessoa {
+    constructor(nome,sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+    falar(){
+        console.log(`${this.nome} está falando`)
+    }
+    comer(){
+        console.log(`${this.nome} está comendo`)
+    }
+    beber(){
+        console.log(`${this.nome} está bebendo`)
+    }
+}
+
+const p1 = new Pessoa ('Saulo','Gomes')
+const p2 = new Pessoa ('Beatriz','Liza')
+console.log(p1,p2)
+p1.falar()
+p2.comer()
