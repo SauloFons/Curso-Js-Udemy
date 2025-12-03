9b7f4ab6105f2135759680ab81611ce54779b644
Saulo <saulo.ftu@gmail.com>
2025-12-03 16:24:10 -0300

    AJAX - XMLHttpRequest

--- AJAX/assets/js/main.js
+++ AJAX/assets/js/main.js
@@ -1,43 +1,46 @@
-const request = (obj) => {
-    const xhr  =  new XMLHttpRequest();
-    xhr.open(obj.method,obj.url,true);
+const request = obj => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
     xhr.send();
 
-    xhr.addEventListener('load', ()=>{
-        if (xhr.status >= 200 && xhr.status < 300){
-            obj.success(xhr.responseText);
-        }else{
-            obj.error(xhr.statusText);
-        }
+    xhr.addEventListener('load', () => {
+      if(xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
     });
+  });
 };
 
-document.addEventListener('click', e =>{
-    const el = e.target;
-    const tag = el.tagName.toLowerCase()
+document.addEventListener('click', e => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
 
-    if( tag === 'a'){
-        e.preventDefault();
-        carregaPagina(el);
-    }
+  if (tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
 });
 
-function carregaPagina(el){
-    const href = el.getAttribute('href')
-    console.log(href)
-    request({
-    method : 'GET',
-    url : href,
-    success(response){
-        carregaResultado(response)
-    },
-    error(errorText){
-        console.log(errorText)
-    }
-})
+async function carregaPagina(el) {
+  const href = el.getAttribute('href');
 
-};
-function carregaResultado(response){
-    const resultado = document.querySelector('.resultado')
-    resultado.innerText = response;
+  const objConfig = {
+    method: 'GET',
+    url: href
+  };
+
+  try {
+    const response = await request(objConfig);
+    carregaResultado(response);
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
 }
