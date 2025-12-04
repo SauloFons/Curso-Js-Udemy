ea919b4170a89a4d242501a86c185cf8d4242091
Saulo <saulo.ftu@gmail.com>
2025-12-04 14:56:40 -0300

    Import e Export ES6 Modules

---
+++ Import_Export_ES6/src/index.js
@@ -0,0 +1,8 @@
+import { nome,sobrenome,idade,soma } from './modulo1'
+import * as moduloCompleto from './modulo1'
+console.log(moduloCompleto)
+console.log(nome)
+
+
+import qualquerNome from './modulo1'
+console.log(qualquerNome(3,1))
---
+++ Import_Export_ES6/src/modulo1.js
@@ -0,0 +1,15 @@
+export const nome = 'Saulo';
+export const sobrenome = 'Gomes'
+export const idade = 30;
+
+export function soma(x,y){
+    return x + y ;
+}
+
+export default function subtracao(x,y){
+    return x- y
+}
+// export {nome , sobrenome , idade, soma};
+
+// export {nome as default , sobrenome , idade, soma};  Sem isso não é possível fazer com que a constante seja padrão
+
