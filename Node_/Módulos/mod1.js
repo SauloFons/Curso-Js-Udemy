5cab138386941d90bc1dd3648e970071c142ff98
Saulo <saulo.ftu@gmail.com>
2025-12-08 11:59:55 -0300

    Módulos node pt1

---
+++ Node_/Módulos/mod1.js
@@ -0,0 +1,20 @@
+// const nome = 'Saulo'
+// const sobrenome = 'Gomes'
+
+// function falaNome(){
+//     console.log(nome,sobrenome)
+// }
+
+// // module.exports.nome = nome;
+// // module.exports.sobrenome = sobrenome;
+// // module.exports.falaNome = falaNome;
+
+// exports.nome = nome;
+// exports.sobrenome = sobrenome;
+// exports.falaNome = falaNome;
+// this.qualquerCoisa = 'O que quiser exportar';
+
+// console.log(module.exports)
+
+
+//****************** AULA 2 *********************
