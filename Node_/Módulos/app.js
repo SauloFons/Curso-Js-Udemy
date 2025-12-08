cc08b1cbf23b96b9ca8c64014343effa1c087abb
Saulo <saulo.ftu@gmail.com>
2025-12-08 13:27:56 -0300

    Módulos node pt2

--- Node_/Módulos/app.js
+++ Node_/Módulos/app.js
@@ -1,2 +1,17 @@
 // const mod1 = require('./mod1');
 // console.log(mod1)
+
+
+
+//****************** AULA 2 *********************
+
+
+
+// const multiplicação = require ('./mod1');
+
+// console.log(multiplicação(5,2))
+
+const Cachorro = require('./mod1')
+
+const dog = new Cachorro('Hani')
+dog.latir()
--- Node_/Módulos/mod1.js
+++ Node_/Módulos/mod1.js
@@ -18,3 +18,18 @@
 
 
 //****************** AULA 2 *********************
+
+// module.exports = function (x,y){
+//     return x * y
+// }
+
+
+module.exports = class Cachorro {
+    constructor(nome){
+        this.nome = nome;
+    }
+
+    latir(){
+        console.log (`${this.nome} está latindo`)
+    }
+}
