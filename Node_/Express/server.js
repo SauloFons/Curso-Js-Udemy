0a6087bd2106609ca7bc2085d9b4a1cad381a832
Saulo <saulo.ftu@gmail.com>
2025-12-12 10:07:12 -0300

    atualização

--- Node_/Express/server.js
+++ Node_/Express/server.js
@@ -4,13 +4,22 @@ const app = express();
 //http://meusite.com/ <- GET
 
 app.get('/', (req,res) =>{
-    res.send('Hello World!')
+    res.send(`
+     <form action = "/" method = "POST">
+     Nome: <input type = "text" name = "nomes" >
+     <button> Enviar </button>
+     </form>   
+     `);
 });
 
-app.get('/contato', (req,res) =>{
-    res.send('Obrigado pelo contato')
+app.post('/', (req,res) =>{
+    res.send('Recebi o fomulário');
 })
+
+app.get('/contato', (req,res) =>{
+    res.send('Obrigado pelo contato!');
+});
 app.listen(3000, () =>{
-    console.log('Servidor executando na porta 3000')
-    console.log('Acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000');
+    console.log('Acessar http://localhost:3000');
 });
